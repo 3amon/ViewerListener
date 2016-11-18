06bcb6de6b0ccd0803bea2fbe578ce9983e66366
Eamon White <white.eamon@gmail.com>
2016-11-17 20:03:04 -0600

    added some test algorithms. https://pypi.python.org/pypi/hdbscan/ may be a more promising approach

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var Promise = require("bluebird");
 var fs = Promise.promisifyAll(require("fs"));
 var path = require('path');
 var csv = Promise.promisifyAll(require('csv'));
+var clusterfck = require("clusterfck");
+var clustering = require('density-clustering');
+
 
 var audioDir = process.env.AUDIO_PROCESS_OUTPUT_DIR;
 
@@ -21,6 +24,14 @@ function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
+function leaves(hcluster) {
+    // flatten cluster hierarchy
+    if(!hcluster.left)
+        return [hcluster];
+    else
+        return leaves(hcluster.left).concat(leaves(hcluster.right));
+}
+
 app.get('/audio-data/', function(req, res) {
 
     fs.readdirAsync(audioDir).filter(endsWith('.wav.csv')).map(function(fileName) {
@@ -28,11 +39,25 @@ app.get('/audio-data/', function(req, res) {
         return Promise.join(contents, function (contents) {
             return {
                 fileName: fileName,
-                contents: contents
+                contents: contents[0].map(function(value){
+                    return parseFloat(value);
+                })
             }
         });
     }).then (function(dataList) {
-        res.send(dataList);
+        var data = [];
+        for(var rowNum = 0; rowNum < dataList.length; ++ rowNum)
+        {
+            var row = [];
+            for(var eleNum = 0; eleNum < dataList[rowNum].contents.length; ++ eleNum)
+            {
+                var x = dataList[rowNum].contents[eleNum];
+                row.push(x);
+            }
+            data.push(row);
+        }
+        var tree = leaves(clusterfck.hcluster(data, "euclidean", "average", 1.0));
+        res.send(tree);
     }).catch(function(e) {
         console.error(e.stack);
     });
@@ -42,6 +67,60 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+function GetDataSet(callback)
+{
+    fs.readdirAsync(audioDir).filter(endsWith('.wav.csv')).map(function(fileName) {
+        var contents = fs.readFileAsync(path.join(audioDir, fileName), 'utf8').then(csv.parseAsync);
+        return Promise.join(contents, function (contents) {
+            return {
+                fileName: fileName,
+                contents: contents[0].map(function(value){
+                    return parseFloat(value);
+                })
+            }
+        });
+    }).then (function(dataList) {
+        callback(dataList);
+    }).catch(function(e) {
+        console.error(e.stack);
+    });
+}
+
+function test2()
+{
+    GetDataSet(function(dataList)
+    {
+        var dataset = dataList.map(function(ele) { return ele.contents} );
+        var optics = new clustering.OPTICS();
+        // parameters: 2 - neighborhood radius, 2 - number of points in neighborhood to form a cluster
+        var clusters = optics.run(dataset, 100, 2);
+        var plot = optics.getReachabilityPlot();
+        console.log(clusters, plot);
+    });
+
+
+}
+
+function test()
+{
+    GetDataSet(function(dataList) {
+        var data = dataList.map(function (ele) {
+            return ele.contents
+        });
+        var dbscan = new clustering.DBSCAN();
+        var distance = 5.0;
+        var noiseFraction = 0.0;
+        while (noiseFraction < 0.05) {
+            console.log("Trying distance: " + distance);
+            var clusters = dbscan.run(data, distance, 2);
+            noiseFraction = dbscan.noise.length / data.length;
+            distance = distance / 2.0;
+        }
+        console.log(clusters, dbscan.noise);
+    });
+}
+
 app.listen(5312, function () {
     console.log('Example app listening on port 5312!');
+    test2();
 });
